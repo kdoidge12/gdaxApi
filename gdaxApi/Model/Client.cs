70bd497f5635886697fe7a900c4d546076fe124b
kdoidge12 <kdoidge12@gmail.com>
2018-05-07 21:43:51 -0400

    Changed the Client to utilize a singleton pattern

--- gdaxApi/Model/Client.cs
+++ gdaxApi/Model/Client.cs
@@ -11,13 +11,30 @@ namespace gdaxApi.Model
         private string apiKey = "ad5a8d1652dba099a2057b3d3c066c93";
         private string secret = "I034PbAsVNTPduY6Uwuf3r4gJs7AuC/ptGqzcq3UTDFgXuHD75xCt8Di8K8eYxlCq/CDjUosT4F40gNf9iczzQ==";
         private string passphrase = "JITA";
+        private static Client instance = null;
         public GDAXSharp.GDAXClient gdaxClient = null;
         public Authenticator authenticator = null;
+        private static readonly object padlock = new object();
 
         public Client()
         {
             this.authenticator = new Authenticator(this.apiKey, this.secret, this.passphrase);
             this.gdaxClient = new GDAXSharp.GDAXClient(authenticator);
+        }
+
+        public static Client Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Client();
+                    }
+                    return Instance;
+                }            
+            }
         }
     }
 }
